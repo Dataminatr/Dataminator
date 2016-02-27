db555acaca5e65325afdcbf285e7fdf8249af713
naveenal <naveenxps@gmail.com>
2016-02-26 18:45:38 -0800

    Corrected the function calls

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.set('port', (process.env.PORT || 9001));
 // To check App status
 app.get('/', function(req, res) {
     res.send('Dataminator is Live yo!');
-    console.log("It's live on port:", port);
 });
 
 
@@ -23,27 +22,27 @@ app.post('/post', function(req, res) {
     var parsedUrl = url.format ({
         pathname : 'https://api.genius.com/search', // API URL ,
         query: {
-            access_token : process.env.GOOGLE_ACCESS,
-            q : req.body.text
+            access_token: process.env.GOOGLE_ACCESS,
+            q: req.body.text
         }
     });
 
-    request(parsedUrl, function (error,req, res) {
+    request(parsedUrl, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             var ans = data.response.hits[0].result.url; //firstendpoint = data.endpoint;
 
             var body = {
-                response_type : "in_channel",
+                response_type: "in_channel",
                 text: ans //firstendpoint
             };
 
         res.send(body);
     }
 
-   });
+  });
 });
 
 app.listen(app.get('post'), function() {
-    console.log('Data app is working on port', app.get('port'));
+    console.log('Dataminator is working on port', app.get('port'));
 });
