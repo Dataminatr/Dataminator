55be00daea96d2f16a7dfb594903b9baff665bee
naveenal <naveenxps@gmail.com>
2016-02-26 17:43:55 -0800

    Slack Skeleton init

--- index.js
+++ index.js
@@ -14,5 +14,30 @@ app.set('port', (process.env.PORT || 9001));
 // To check App status
 app.get('/', function(req, res) {
     res.send('Dataminator is Live yo!');
-    consle.log("It's live on port:", port );
+    console.log("It's live on port:", port);
+});
+
+app.post('/post', function(req, res) {
+    var parsedUrl = url.format ({
+        pathname = // API URL ,
+        query: {
+            access_token : process.env.GOOGLE_ACCESS
+            query : req.body.text
+        }
+    });
+
+    request(parsedUrl, function (error,req, res) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            var //firstendpoint = data.endpoint;
+
+            var body = {
+                response_type : "in_channel",
+                text: //firstendpoint
+            };
+
+        res.send('body');
+    }
+
+   });
 });
