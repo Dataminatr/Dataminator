fbf4b0eac0da5cdb764884d5d3725b852712f378
naveenal <naveenxps@gmail.com>
2016-02-26 17:08:11 -0800

    App on index

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
-var url = require('url');
-var request = require('request');
+var router = express.Router();
 
-var slackRouter =  require('./routers/slack.js')
+
+// For JSON Parsing
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true }));
+
+// Setting the Router at Port 9001
+app.set('port', (process.env.PORT || 9001));
+
+// To check App status
+app.get('/', function(req, res) {
+    res.send('Dataminator is Live yo!');
+});
--- routers/slack.js
+++ routers/slack.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-
-//enter code here
-//router.get('/', function(req, res){})
-
-
-
-
-module.exports = router;
+// var express = require('express');
+// var app = express();
+// var router = express.Router();
+//
+// var body-parser = reuire('bodyParser');
+// app.use(bodyParser.JSON());
+// app.use(bodyParser.urlencoded({extended : true }));
+//
+// //enter code here
+// //router.get('/', function(req, res){})
+//
+//
+//
+// 
+// module.exports = router;
