f86fa89acf8e787e1ee5c478850fcc418e1e1524
Ron Lapushner <lapushnerron@gmail.com>
2016-02-27 00:03:58 -0800

    rearranged index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 process.env.GOOGLE_APPLICATION_CREDENTIALS = '/home/ronll/Documents/Dataminator/config/testVisionApi-02064f733b63.json';
+app.set('port', (process.env.PORT || 9001));
 
 var express = require('express');
 var app = express();
@@ -6,8 +7,6 @@ var slackRoutes = require(__dirname + '/routers/slackRouter.js')
 var googleVision = require(__dirname + '/controllers/googleVision.js')
 
 app.use('/slack', slackRoutes);
-
-app.set('port', (process.env.PORT || 9001));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
