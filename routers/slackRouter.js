d8e0967afe209f9b61f752c654ac87fabcd2fd96
Ron Lapushner <lapushnerron@gmail.com>
2016-02-27 04:45:05 -0800

    fixed text detection

--- routers/slackRouter.js
+++ routers/slackRouter.js
@@ -5,6 +5,8 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var https = require('https');
 var fs = require('fs');
+var googleVision = require(__dirname + '/../controllers/googleVision.js')
+
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +21,9 @@ router.post('/', function(req, res){
 
     res.on('end', function() {
       console.log(image);
+      googleVision.visionImage( image, 'TextDetection', function(err,result){
+	console.log(result, 'text detection');
+      })
     })
   });
 });
