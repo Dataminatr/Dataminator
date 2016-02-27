1e0301b914f838f0a72c2e3379a56224a60fa405
Ron Lapushner <lapushnerron@gmail.com>
2016-02-26 22:03:03 -0800

    added google vision controller

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+process.env.GOOGLE_APPLICATION_CREDENTIALS = '/home/ronll/Documents/Dataminator/modules/../config/testVisionApi-02064f733b63.json';
+
 var express = require('express');
 var app = express();
-
 var slackRouter =  require('./routers/slack.js')
+var googleVision = require(__dirname + '/controllers/googleVision.js')
 
+googleVision.visionImage( 'ronimage/IMG_0191.JPG', 'LabelDetection', function(err,result){ 
+  console.log(result);
+})
