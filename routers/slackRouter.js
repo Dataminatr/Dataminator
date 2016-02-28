685a34a0bd0eb8ff16f3db024e5477a63da9bd3e
Naveen <Naveenal@users.noreply.github.com>
2016-02-28 14:49:17 -0800

    Changed the Placeholder text

--- routers/slackRouter.js
+++ routers/slackRouter.js
@@ -17,31 +17,31 @@ router.post('/', function(req, res){
   var userId = req.body.user_id;
   var responseURL = req.body.response_url;
 
-  res.send('processing request ...');
+  res.send('Docufy is performing its magic .. ');
 
   googleVision.visionImage(imageURL, 'TextDetection', function(err,result){
 
     var text = result.responses[0].textAnnotations[0].description;
     var link = officeDocs(text);
-    var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et felis libero. Pellentesque habitant morbi tristique senectus et netus et."
+    var lorem = "Your digitized file is here.."
 
     var options = {
       uri: responseURL,
     method: 'POST',
     json: {
       "response_type": "in_channel",
-    "username": "Dataminator",
+    "username": "Docufy",
     "attachments": [
   {
     "color": "#1aa3ff",
-    "title": "Document Download",
+    "title": "Ready for Download",
     "title_link": link,
     //"author_name": 'Courtesy of <@'+ userId + '|' + userName + '>',
     "thumb_url": "http://johnprados.com/wp-content/uploads/2013/08/word.png",
-    "text": lorem,
+    "text": "Your digitized file is here, You can collaborate with others!",
     "fields": [
     {
-      "title": "Courtesy Of",
+      "title": "Document created by",
       "value": '<@'+ userId + '|' + userName + '>',
       "short": false
     }
